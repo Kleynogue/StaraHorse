59ee044a3fa0e0c5a12d7186118cdc1788ade898
Kleysber <Kleynogue08@gmail.com>
2023-01-07 19:31:54 -0400

    Corrección en los create e inicio de los reportes

---
+++ server/controllers/reports.js
@@ -0,0 +1,50 @@
+const {pool} = require('../settings/pg');
+const reports = require('../models/reports');
+
+function getUsers(){
+
+};
+
+function getStuds_1(){
+
+};
+
+function getStuds_2(){
+
+};
+
+function getStuds_3(){
+
+};
+
+function getSpecimen(){
+
+};
+
+function getImplements(){
+
+};
+
+function getTrainers(){
+
+};
+
+function getJockey(){
+
+};
+
+function getRestaurants(){
+
+};
+
+module.exports = {
+    getUsers,
+    getStuds_1,
+    getStuds_2,
+    getStuds_3,
+    getSpecimen,
+    getImplements,
+    getTrainers,
+    getJockey,
+    getRestaurants
+}
